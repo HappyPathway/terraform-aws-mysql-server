da0552f0dcffe51fa58164c338f7b82c7cee0346
Dave Arnold <darnold@hashicorp.com>
2018-08-09 15:01:26 -0700

    skipping final snapshot

--- main.tf
+++ main.tf
@@ -10,4 +10,5 @@ resource "aws_db_instance" "default" {
   parameter_group_name = "default.mysql5.7"
   publicly_accessible = true
   vpc_security_group_ids = ["${aws_security_group.db.id}"]
+  skip_final_snapshot = true
 }
