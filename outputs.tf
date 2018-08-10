a261d6efa5aae42d070f2ae35ddcf120e846d3da
Dave Arnold <darnold@hashicorp.com>
2018-08-09 17:29:08 -0700

    adding credential endpoints to outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "db_name" {
 output "vault_mount" {
   value = "${vault_mount.db.path}"
 }
+
+output "mysql_crud" {
+  value = "${var.service_name}/db-${var.db_name}/creds/mysql_crud"
+}
+
+output "mysql_ro" {
+  value = "${var.service_name}/db-${var.db_name}/creds/mysql_ro"
+}
