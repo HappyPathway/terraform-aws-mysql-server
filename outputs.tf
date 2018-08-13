04aa0cd59b963122a773f1035564c53faf5f344e
Dave Arnold <darnold@hashicorp.com>
2018-08-13 11:59:35 -0700

    blanking vault config

--- outputs.tf
+++ outputs.tf
@@ -5,15 +5,3 @@ output "endpoint" {
 output "db_name" {
   value = "${var.db_name}"
 }
-
-output "vault_mount" {
-  value = "${vault_mount.db.path}"
-}
-
-output "mysql_crud" {
-  value = "${var.service_name}/db-${var.db_name}/creds/mysql_crud"
-}
-
-output "mysql_ro" {
-  value = "${var.service_name}/db-${var.db_name}/creds/mysql_ro"
-}
