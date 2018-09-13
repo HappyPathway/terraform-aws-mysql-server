be2880195b71ce7d5930659e43a1800a4c7c8237
David Arnold <dja+djaboxx@sonic.net>
2018-09-13 13:05:04 -0700

    Update mysql_crud.sql

--- vault_policy_templates/mysql_crud.sql
+++ vault_policy_templates/mysql_crud.sql
@@ -1,4 +1,4 @@
 CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';
-GRANT ALTER, CREATE ON ${var.db_name} to '{{name}}'@'%'; 
-grant CREATE,DROP,SELECT,INSERT,UPDATE,DELETE on ${var.db_name}.* to '{{name}}'@'%'; 
+GRANT ALTER, CREATE ON ${db_name} to '{{name}}'@'%'; 
+grant CREATE,DROP,SELECT,INSERT,UPDATE,DELETE on ${db_name}.* to '{{name}}'@'%'; 
 flush privileges
