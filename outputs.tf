2edeeeab81ee14268b07a226da14157b94a6cbf7
David Arnold <dja+djaboxx@sonic.net>
2018-09-17 22:39:12 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@ output "endpoint" {
   value = "${aws_db_instance.default.endpoint}"
 }
 
+output "service_name" {
+  value = "${var.service_name}"
+}
+
+output "address" {
+  value = "${aws_db_instance.default.endpoint}"
+}
+
 output "db_name" {
   value = "${var.db_name}"
 }
